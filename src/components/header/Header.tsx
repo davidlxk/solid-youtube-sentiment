83a97043717e81498bb7230144809cae1eb027b9
David Lin <david.b.lin@accenture.com>
2022-10-24 00:21:46 +0800

    revert bg color

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 const Header: Component<HeaderProps> = (props:HeaderProps) => {
 
     return(
-        <div class="h-20 bg-red-400 flex flex-row fixed top-0 border-b border-blue-100">
+        <div class="h-20 bg-white flex flex-row fixed top-0 border-b border-blue-100">
             <input type="text" class="w-screen h-10 mt-8 text-2xl outline-none transform transition-all duration-700 ease-in-out focus:pl-5"
                 placeholder='Enter/Paste a youtube link here'
                 oninput={(e)=>props.formatURL(e.currentTarget.value)}
